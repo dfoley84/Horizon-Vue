9e9a712f6cf03a6edc852f9524071a085090e8f0
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2023-02-09 10:28:04 +0000

    Project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Horizon-Vue
+![alt text](https://github.com/[username]/[reponame]/blob/[branch]/2023-02-09 10_20_04-frontend.png?raw=true)
+
+# Actions Required
+    1) PowerShell Script to pull Horizon VDI from Horizon to Database, 
+    2) Create Jenkins Free Style Jobs for Each PowerAction
+    3) Configure RabbitMQ to Take PowerAction Required 
+    4) Python Script to Get Message and Trigger Remote Jenkins Job
+
+
---
+++ vsphere/backend/main.py
@@ -0,0 +1,57 @@
+from fastapi import Depends, FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from database import SessionLocal
+import models
+import json
+
+app = FastAPI()
+origins = [
+ "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("shutdown")
+def shutdown_event():
+    #Close Any Open Connections
+    db = SessionLocal()
+    db.close()
+
+#Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+#Search for a specific User
+@app.post("/searchdata")
+async def HorizonServers(
+    request: Request,
+    db: Session = Depends(get_db)
+): 
+    response_object = {'status': 'success'}
+    data = await request.json()
+    user = data['TitleUser']
+    print(user)
+    response_object['SearchvDesks']  = db.query(models.Horizon).filter(models.Horizon.UserName == user).all()
+    return JSONResponse(content=jsonable_encoder(response_object))
+
+
+@app.post("/powercycle")
+async def selectedMachine(item: dict):
+    message = json.loads(json.dumps(item, indent=2))
+    print(message['data']['PowerCycle'])
+    print(message['data']['vDesk']['MachineName'])
+    return JSONResponse(content=jsonable_encoder({'status': 'success'}))
