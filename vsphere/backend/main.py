9570768340722c8a09322e3859f40e5f8e1ae419
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2023-09-24 16:23:45 +0100

    Update main.py

--- vsphere/backend/main.py
+++ vsphere/backend/main.py
@@ -3,14 +3,6 @@ import paramiko
 import asyncio
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
-<<<<<<< HEAD
-=======
-from sqlalchemy.orm import Session
-from database import SessionLocal
-from rabbitmq import RabbitMQ_Sender
-import models
-import json
->>>>>>> a60a5bbb8959cfd0a64439707f8870d35754a069
 
 from horizon import router as HorizonRouter
 from powercycle import router as PowerCycleRouter
@@ -43,7 +35,7 @@ async def websocket_endpoint(websocket: WebSocket):
 
     try:
         print("Before SSH connect")
-        ssh_client.connect('', port=22, username='', password='')  #Needs to be updated to pass IP Address, Username from DB Query
+        ssh_client.connect('', port=22, username='', password='') # Needs to be updated to get Parameters from DB 
         print("Connected to SSH")
     except Exception as e:
         logging.error(f"Error connecting to SSH: {e}")
@@ -71,42 +63,4 @@ async def websocket_endpoint(websocket: WebSocket):
         logging.error(f"Error during WebSocket communication: {e}")
         await websocket.send_text(f"Error: {e}")
     finally:
-<<<<<<< HEAD
         ssh_client.close()
-=======
-        db.close()
-
-#Search for a specific User
-@app.get("/horizon")
-async def HorizonServers(
-    db: Session = Depends(get_db)
-): 
-    response_object = {'status': 'success'}
-    response_object['vdesks']  = db.query(models.Horizon).all()
-    return JSONResponse(content=jsonable_encoder(response_object))
-
-#Search for a specific User
-@app.post("/searchdata")
-async def HorizonServers(
-    request: Request,
-    db: Session = Depends(get_db)
-): 
-    response_object = {'status': 'success'}
-    data = await request.json()
-    user = data['TitleUser']
-    print(user)
-    response_object['SearchvDesks']  = db.query(models.Horizon).filter(models.Horizon.UserName == user).all()
-    return JSONResponse(content=jsonable_encoder(response_object))
-
-
-@app.post("/powercycle")
-async def selectedMachine(item: dict):
-    message = json.loads(json.dumps(item, indent=2))
-  
-  
-    print(message['data']['PowerCycle'])
-    print(message['data']['vDesk']['MachineName'])
-    
-    RabbitMQ_Sender(message) #Passing Message to Class RabbitMQ_Sender
-    return JSONResponse(content=jsonable_encoder({'status': 'success'}))
->>>>>>> a60a5bbb8959cfd0a64439707f8870d35754a069
