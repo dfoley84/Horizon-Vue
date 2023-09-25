03c3551eb47ec369a133a9ec8b365f8ec7c604ea
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2023-09-25 09:21:35 +0100

    Add files via upload

--- vsphere/frontend/src/components/Header/NavigationBar/Navigation.vue
+++ vsphere/frontend/src/components/Header/NavigationBar/Navigation.vue
@@ -1,9 +1,10 @@
 <template>
-    <nav class="column is-2 menu">
-      <p class="menu-label">Menu</p>
-      <ul class="menu-list">
-        <router-link to="/">Horizon</router-link>
-        <router-link to="/search">Search User vDesk</router-link>
-      </ul>
-    </nav>
-  </template>
+  <nav class="column is-2 menu">
+    <p class="menu-label">Menu</p>
+    <ul class="menu-list">
+      <router-link to="/">Horizon</router-link>
+      <router-link to="/search">Search User vDesk</router-link>
+      <router-link to="/build">Build vDesk</router-link>
+    </ul>
+  </nav>
+</template>
