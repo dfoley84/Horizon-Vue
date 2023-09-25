9488991142588a46d709cbc4e05ba8c46300cc76
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2023-09-25 09:18:48 +0100

    Add files via upload

---
+++ vsphere/frontend/src/views/buildView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="form-wrapper">
+      <form @submit.prevent="handleSubmit">
+        <div class="form-group">
+          <label for="requester">Requester:</label>
+          <input type="text" id="requester" v-model="form.requester" />
+        </div>
+
+        <div class="form-group">
+          <label for="machinename">Machine Name:</label>
+          <input type="text" id="machinename" v-model="form.machinename" />
+        </div>
+
+        <div class="form-group">
+          <label for="server">Server:</label>
+          <input type="text" id="server" v-model="form.server" />
+        </div>
+
+        <div class="form-group">
+          <label for="poolname">Pool Name:</label>
+          <input type="text" id="poolname" v-model="form.poolname" />
+        </div>
+
+        <button class="submit-btn" type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        requester: '',
+        poolname: '',
+        machinename: '',
+        server: ''
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.form);  // Here you can process the form data
+    }
+  }
+}
+</script>
