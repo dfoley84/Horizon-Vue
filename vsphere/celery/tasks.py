e285428c78eb995c2f0163ef74857fca23bd0fda
dfoley84 <83467582+dfoley84@users.noreply.github.com>
2023-09-27 11:07:38 +0100

    Update tasks.py

--- vsphere/celery/tasks.py
+++ vsphere/celery/tasks.py
@@ -1,1 +1,23 @@
+from celery import Celery
+from jenkins import jenkinsbuild  
+
+# Configure Celery app
+app = Celery('tasks', broker='pyamqp://guest:guest@localhost/vsphere')
+
+@app.task
+def print_message(body):
+    print(f"Received message: {body}")
+
+    # Extract the contents of the message
+    try:
+        content = body.get('currentUser', {})
+        for key, value in content.items():
+            print(f"{key}: {value}")
+              #Call the Jenkinsbuild python Script 
+        
+      
+
+
+    except Exception as e:
+        print(f"Error parsing message: {e}")
 
